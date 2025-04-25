759f92b9816898051afb6ac8293c503a6ca5e7df
hudsontb <hudsonthomasborges@gmail.com>
2025-04-25 22:55:31 +0000

    missão do site descrita na página `em_construcao`

--- pages/em_construcao.js
+++ pages/em_construcao.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Página em construção.</h1>;
+  return (
+    <h1>
+      Não sei ao certo como vou montar este site, mas tentarei de alguma maneira
+      trazer notícias e informações, de forma simples, sobre o que ocorre no
+      mundo, ao mesmo tempo em que tento também espalhar o Evangelho de Jesus
+      Cristo. Pois com A Palavra e visão cristã conseguimos nos orientar diante
+      o caos e as trevas do mundo.
+    </h1>
+  );
 }
 
 export default Home;
