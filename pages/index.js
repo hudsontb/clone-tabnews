e2b4efd8f46fa7a0575acf53eb5298bc7c8babe5
hudsontb <hudsonthomasborges@gmail.com>
2025-04-27 23:20:18 +0000

    atualização da página `index`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>2x Isso aí!!!</h1>;
+  return (
+    <h1>
+      Existem padres que renunciam a tudo e vão para o deserto -- onde não há
+      distração nenhuma a que recorrer -- para desafiar os próprios pensamentos
+      e demônios. Existem pessoas que enlouquecem ao realizar isso, na verdade,
+      nem precisa chegar a esse ponto para causar isso nas pessoas: com muito
+      menos já se consegue.
+    </h1>
+  );
 }
 
 export default Home;
