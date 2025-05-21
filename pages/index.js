5766c14eac9dcca81641f40c60a742ff74c50949
hudsontb <hudsonthomasborges@gmail.com>
2025-05-21 12:24:53 +0000

    adicionar arquivo `compose.yaml` de configurações com o serviço de database

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,14 @@ function Home() {
     <h1>
       Existem padres que renunciam a tudo e vão para o deserto -- onde não há
       distração nenhuma a que recorrer -- para desafiar os próprios pensamentos
-      e demônios. Existem pessoas que enlouquecem ao realizar isso, na verdade,
-      nem precisa chegar a esse ponto para causar isso nas pessoas: com muito
-      menos já se consegue.
+      e aqueles que foram expulsos do Céu. A maioria das pessoas enlouqueceriam
+      ao presenciar poucos momentos dessa situação, pois estão sempre fugindo;
+      sempre anestesiados demais com distrações que nem sabem mais o que estão
+      fazendo: estão perdidos. Em um desses desafios, o padre do deserto
+      perguntou à antiga serpente: <br></br>- Em que os seres humanos são
+      melhores que os anjos caídos? - Em nada vocês nos superam. Nós somos
+      melhores em tudo, mas tem algo que apenas os humanos possuem: a humildade.
+      Isto nós não temos de jeito nenhum.
     </h1>
   );
 }
