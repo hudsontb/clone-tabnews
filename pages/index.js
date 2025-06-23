8d2cde6d810e168724524da45665a96e2fb67bc4
hudsontb <hudsonthomasborges@gmail.com>
2025-06-23 03:08:10 +0000

    alterando texto do arquivo `index.js`

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ function Home() {
     <h1>
       Existem padres que renunciam a tudo e vão para o deserto -- onde não há
       distração nenhuma a que recorrer -- para desafiar os próprios pensamentos
-      e aqueles que foram expulsos do Céu. A maioria das pessoas enlouqueceriam
+      e àqueles que foram expulsos do Céu. A maioria das pessoas enlouqueceria
       ao presenciar poucos momentos dessa situação, pois estão sempre fugindo;
       sempre anestesiados demais com distrações que nem sabem mais o que estão
       fazendo: estão perdidos. Em um desses desafios, o padre do deserto
       perguntou à antiga serpente: <br></br>- Em que os seres humanos são
-      melhores que os anjos caídos? - Em nada vocês nos superam. Nós somos
-      melhores em tudo, mas tem algo que apenas os humanos possuem: a humildade.
-      Isto nós não temos de jeito nenhum.
+      melhores que os anjos caídos? <br></br>- Em nada vocês nos superam. Nós
+      somos melhores em tudo, mas tem algo que apenas os humanos possuem: a
+      humildade. Isto nós não temos de jeito nenhum - Respondeu a serpente.
     </h1>
   );
 }
